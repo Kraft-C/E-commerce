1496515946b684d0db8c48a1cb81861e4ba0e1ea
Kraft-C <clementkraft77160@gmail.com>
2025-11-13 08:50:02 +0100

    first commit

---
+++ src/main/kotlin/org/ldv/ecommerce/ECommerceApplication.kt
@@ -0,0 +1,11 @@
+package org.ldv.ecommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ECommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ECommerceApplication>(*args)
+}
