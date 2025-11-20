eddaa62ba013b2d7cf73ca71cad9501fdad09301
Kraft-C <clementkraft77160@gmail.com>
2025-11-20 12:00:45 +0100

    sprint 3 etape 5

--- src/main/kotlin/org/ldv/ecommerce/ECommerceApplication.kt
+++ src/main/kotlin/org/ldv/ecommerce/ECommerceApplication.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class ECommerceApplication
 
-
 fun main(args: Array<String>) {
     runApplication<ECommerceApplication>(*args)
 }
