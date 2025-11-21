c198d814965af7f8e9a494389d25883830653e7a
Louis3701 <louis.caron16@gmail.com>
2025-11-21 17:17:43 +0100

    mise en place des images

--- src/main/kotlin/org/ldv/ecommerce/controller/admincontrollers/AdminController.kt
+++ src/main/kotlin/org/ldv/ecommerce/controller/admincontrollers/AdminController.kt
@@ -2,6 +2,7 @@ package com.example.votreProjet.controller;
 
 import org.ldv.ecommerce.model.dao.CategorieDAO
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -17,4 +18,16 @@ public class AdminControllerController {
     @GetMapping("/E-commerce/admin/")
     fun adminCategories() = "admin"
 
+    @GetMapping("/e-commerce/admin/categories")
+    fun index(model: Model):String {
+        val categorieDAO = null
+        val categories = categorieDAO.findAll()
+        model.addAttribute("categories", categories)
+        return "pageAdmin/categorie/index"
+    }
+
+}
+
+private fun Nothing?.findAll() {
+    TODO("Not yet implemented")
 }
